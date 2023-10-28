b65c51e76460f2f3d4252bcd13c2a032ce1d1b93
Vitor de Paula <vitordepaula98@hotmail.com>
2023-10-28 20:33:00 -0300

    adicionando Search e Pagination, criado paginas para editar e excluir uma fatura

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,25 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id
+  const invoice = await fetchInvoiceById(id)
+  const customers = await fetchCustomers()
+
+  return (
+    <main>
+      <Breadcrumbs 
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  )
+}
